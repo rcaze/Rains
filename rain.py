565e28eff8702745c67ee8de541309b2c3cef32d
rcaze <romain.caze@gmai.com>
2019-11-22 14:32:28 +0100

    Now for each episodes you can a set of sounds

--- rain.py
+++ rain.py
@@ -11,14 +11,14 @@ def build_drops(downv, upv, directory= "/home/rcaze/Documents/Musique/",
         drops.append(AudioSegment.from_wav(directory+keyword+str(i)+".wav"))
     return drops
 
-
-def rain(durs, ndrops, drops):
+def rain(durs, ndrops, dropss):
     """
     Generate rain sound with different length of time and concatenate them
     """
     out = AudioSegment.silent(duration=sum(durs))
     for i, dur in enumerate(durs):
         for ndrop in range(ndrops[i]):
+            drops = dropss[i]
             n = random.randint(len(drops))
             drop = drops[n]
             out = out.overlay(drop,
@@ -31,10 +31,8 @@ def last():
     pass
 
 if __name__ == "__main__":
-    durs = [1000 for i in range(20)]
-    ndrops = [i*2 for i in range(20)]
-    drops = build_drops(0, 3, keyword="Si")
-    out = rain(durs, ndrops, drops)
-    out.export("Rains/BsComingRain.wav", format="wav")
-
-
+    durs = [5000 for i in range(8)]
+    ndrops = [50 for i in range(8)]
+    dropss = [build_drops(45+3*i, 45+3*(i+1)) for i in range(8)]
+    out = rain(durs, ndrops, dropss)
+    out.export("Rains/BsScaleRain.wav", format="wav")
