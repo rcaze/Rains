6382b2d5a0c8edde88097ed75e35e5bc02c9aeee
rcaze <romain.caze@gmai.com>
2019-11-28 10:32:09 +0100

    Cleaning in the script

--- rain.py
+++ rain.py
@@ -1,38 +1,42 @@
 from pydub import AudioSegment
 from numpy import random
+import numpy as np
 
-def build_drops(downv, upv, directory= "/home/rcaze/Documents/Musique/",
-                keyword = "Enregistrement_"):
+def build_drops(drop_multp, drops):
     """
-    Generate a list of drop sounds from  catalogs of wav files
+    Multiply the sounds by the number of time you want to hear the a drop sound
     """
-    drops = []
-    for i in range(downv, upv):
-        drops.append(AudioSegment.from_wav(directory+keyword+str(i)+".wav"))
-    return drops
+    return drops * drop_multp
 
-def rain(durs, ndrops, dropss):
+
+def rain_core(dur, drops):
     """
     Generate rain sound with different length of time and concatenate them
     """
-    out = AudioSegment.silent(duration=sum(durs))
-    for i, dur in enumerate(durs):
-        for ndrop in range(ndrops[i]):
-            drops = dropss[i]
-            n = random.randint(len(drops))
-            drop = drops[n]
-            out = out.overlay(drop,
-                              position=random.randint(sum(durs[:i]),
-                                                      sum(durs[:i+1])))
+    out = AudioSegment.silent(duration=dur)
+    pos = random.randint(0, dur, len(drops))
+    for i, drop in enumerate(drops):
+        out = out.overlay(drop, position=pos[i])
     return out
+
+
+def match_target_amplitude(sound, target_dBFS):
+    """
+    Normalize a sound to avoid saturation
+    """
+    change_in_dBFS = target_dBFS - sound.dBFS
+    return sound.apply_gain(change_in_dBFS)
 
 
 def last():
     pass
 
 if __name__ == "__main__":
-    durs = [5000 for i in range(8)]
-    ndrops = [50 for i in range(8)]
-    dropss = [build_drops(45+3*i, 45+3*(i+1)) for i in range(8)]
-    out = rain(durs, ndrops, dropss)
-    out.export("Rains/BsScaleRain.wav", format="wav")
+    #drops = [AudioSegment.from_wav('Enregistrement_%i.wav'%i) for i in range(97, 104)]
+    #
+    gain_r = 5
+    drops = [AudioSegment.from_file('samples/La0.wav', channel=i) for i in range(gain_r)]
+    drops = build_drops(20, drops)
+    out = rain_core(8000, drops)
+    out = match_target_amplitude(out, -30)
+    out.export("rains/LaPluie.wav", format="wav")
